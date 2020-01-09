355404fc17dc6dc965ecd9bf8be0a6472e5794a8
Justin Maher <justin.maher@appfolio.com>
2020-01-08 16:40:50 -0800

    fixup! add images resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
+
+  get '/add_image_form', to: 'application#add_image'
+
+  resources :images
 end
+

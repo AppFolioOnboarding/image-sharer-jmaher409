d546e6e4d77a0bd2355e4ec5b62c37cf76f3e3c7
Justin Maher <justin.maher@appfolio.com>
2020-01-14 10:16:54 -0800

    fixup! fixup! add flash message
    Tue Jan 14 10:16:54 PST 2020

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,3 @@
-require 'uri'
-
 class ImagesController < ApplicationController
 
   def new; end
@@ -10,33 +8,18 @@ class ImagesController < ApplicationController
   end
 
   def create
-    url = params[:url]
-
-    redirect_to new_image_path, notice: 'Invalid url' unless valid_url(url)
-
-    @image = Image.new(create_image_params)
-    @image.save
-
-    redirect_to @image
+    render plain: params[:url].inspect
+    #@image = Image.new(create_image_params)
+    #@image.save
+    #
+    #redirect_to @image, notice: 'Image created successfully' if @image.valid?
   end
 
   private
-
-  def show_image_params
-    params.permit(:id, :url)
-  end
 
   def create_image_params
     params.permit(:url)
   end
 
-  def valid_url(url)
-    parsed = URI.parse(url)
-    print 'parsed', parsed
-    parsed.is_a?(URI::HTTP) && !parsed.host.nil?
-  rescue URI::InvalidURIError
-    print 'invalid url: ', url
-    false
-  end
 end
 
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,26 @@
 require 'uri'
 
+class UrlValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    print 'value' + value
+    record.errors[:url] << 'Invalid Url.' unless valid_url(value)
+  end
+
+  private
+
+  def valid_url(url)
+    print 'in valid_url urL=' + url
+    parsed = URI.parse(url)
+    parsed.is_a?(URI::HTTP) && !parsed.host.nil?
+  rescue URI::InvalidURIError
+    print 'invalid Url'
+    false
+  end
+end
+
 class Image < ApplicationRecord
+  include ActiveModel::Conversion
+  extend  ActiveModel::Naming
+
+  validates_with UrlValidator, attributes: [:url]
 end

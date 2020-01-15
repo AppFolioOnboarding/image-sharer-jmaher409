7a3e3430eb711d8071d5cea0812bb85a69760560
Justin Maher <justin.maher@appfolio.com>
2020-01-15 11:29:31 -0800

    jmm - create welcome controller

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get welcome_index_url
     assert_response :success
+    assert_select 'h1', 1, text: 'Image Library'
   end
-
 end

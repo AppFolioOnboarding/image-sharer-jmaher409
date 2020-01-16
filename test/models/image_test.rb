c3a35b8349460344590ac16192f8e6b3d7f798a8
Justin Maher <justin.maher@appfolio.com>
2020-01-16 10:39:31 -0800

    jmm - acts-as-taggable-on gem and migrations

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -16,4 +16,15 @@ class ImageTest < ActiveSupport::TestCase
     assert_not image.valid?
     assert_equal ['is not a valid URL'], image.errors.messages[:url]
   end
+
+  test 'tag_list for image should be empty if no tags were added' do
+    image = Image.new(url: 'foo')
+    assert_equal [], image.tag_list
+  end
+
+  test 'tag_list returns added tags' do
+    image = Image.new(url: 'foo')
+    image.tag_list.add('foo, bar', parse: true)
+    assert_equal %w[foo bar], image.tag_list
+  end
 end

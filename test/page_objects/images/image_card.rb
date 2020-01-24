99ac99fc2d8a1213e8cf74932d117cb44166d61a
Justin Maher <justin.maher@appfolio.com>
2020-01-24 10:21:46 -0800

    jmm - add page objects for crud test

--- test/page_objects/images/image_card.rb
+++ test/page_objects/images/image_card.rb
@@ -1,16 +1,20 @@
 module PageObjects
   module Images
     class ImageCard < AePageObjects::Element
+      element :image, locator: '.index-image'
+      element :tag_list, locator: '.tag-list'
+
       def url
-        node.find('img')[:src]
+        node.find('.index-image')[:src]
       end
 
       def tags
-        # TODO
+        node.all('.image-tag').map(&:text)
       end
 
       def click_tag!(tag_name)
-        # TODO
+        node.find("#tag_link_#{tag_name}").click
+        window.change_to(IndexPage)
       end
     end
   end
